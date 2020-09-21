8d6b78532ea0a5aebf3affe5b84c4502ca139702
Cesar Martinez <cesaguma@hotmail.com>
2020-09-20 23:34:40 -0400

    Creating a function to change style of each according to the time of day

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,46 @@
-$("#currentDay").text(moment().format('dddd LL'));
+
+$("#currentDay").text(moment().format('dddd , LL'));
+
+$(document).ready(function(){
+
+updateRowTime();
+updateData();
+
+$(".saveBtn").on("click", function(){
+    var data = $(this).siblings(".description").val();
+    var hour = $(this).parents(".time-block").attr("id").split("-")[1];
+    localStorage.setItem(hour,data);  
+});
+
+
+});
+
+function updateRowTime(){
+    var currentHour = 16; //moment().hour();
+
+    console.log(currentHour);
+
+    $(".time-block").each(function(){
+        var rowHour = parseInt($(this).attr("id").split("-")[1]);
+        console.log(rowHour);  
+        if (rowHour < currentHour) {
+            $(this).addClass("past");
+        } else if (rowHour === currentHour) {
+            $(this).addClass("present");
+
+        } else {
+            $(this).addClass("future"); 
+        }
+    });
+
+};
+
+function updateData() {
+    $(".time-block").each(function(){
+        var rowHour = parseInt($(this).attr("id").split("-")[1]);
+        $(this).children(".description").val(localStorage.getItem(rowHour));
+        
+    });
+};
+
 
